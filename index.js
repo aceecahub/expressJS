6e0dba8c472cb576ecfa85488ded915b6895e1c1
aceecahub <itecot02@gmail.com>
2026-02-18 08:35:34 +0700

    repeat

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 import express from "express";
 import userRoutes from "./routes/user.routes.js";
-import path from "path";
 
-const __dirname = path.resolve();
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Hello World");    
-})
-
-app.get('/pages', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages/users/index.html'));
-})
-
-app.get('/create', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages/users/create.html'));
+    res.send("Hello World");
 })
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use('/users', userRoutes);
 
 app.listen(port, () => {
